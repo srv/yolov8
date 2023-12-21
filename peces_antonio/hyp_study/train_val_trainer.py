b1f6e77c8cc78344874574848ba384a4b70e8ef5
Caterina1996 <caterinamusi96@gmail.com>
2023-12-21 12:01:44 +0100

    Updated hyp study files

--- peces_antonio/hyp_study/train_val_trainer.py
+++ peces_antonio/hyp_study/train_val_trainer.py
@@ -6,8 +6,8 @@ from datetime import datetime, timedelta
 from generate_hyp import combination_generator, generate_yaml
 
 project_name = "hyps_study"
-project_path = r"C:\Users\Uib\yolov8\peces_antonio\hyp_study"
-data_path = r"C:\Users\Uib\yolov8\peces_antonio\hyp_study\dataset\data.yaml"
+project_path = r"C:\Users\haddo\yolov8\peces_antonio\hyp_study"
+data_path = r"C:\Users\haddo\yolov8\peces_antonio\dataset\data.yaml"
 model_size = "x"
 batch = 8
 seed=42
@@ -43,3 +43,11 @@ for combination in combinations:
     sleep_time = 5 #min
     print(f"Me despierto a las {(datetime.now() + timedelta(minutes=sleep_time)).strftime('%H:%M:%S')} :(")
     time.sleep(sleep_time*60)
+
+
+cfg_path = r"C:\Users\haddo\yolov8\peces_antonio\configs\no_da.yaml"
+yolo_name = "no_da"
+instruction = f"python ../clearml_log_yolov8.py --project_name {project_name} --task_name {yolo_name} \
+                    --model_size {model_size} --dataset {data_path} \
+                        --epochs {epochs} --batch {batch} --patience {patience} --yolo_proj {project_path} --yolo_name {yolo_name} \
+                            --seed {seed} --lr {lr} --optimizer {optimizer} --config {cfg_path}" 
