2210c9b42b0b3d0df3ffad9973dc935f9be8d043
Caterina1996 <caterinamusi96@gmail.com>
2024-03-26 09:15:58 +0100

    Added fold_idx column

--- peces_antonio/new_dataset/group_fold_validation.py
+++ peces_antonio/new_dataset/group_fold_validation.py
@@ -13,14 +13,15 @@ project_path = args.project_path
 
 # project_path = r"C:\Users\haddo\yolov8\peces_antonio\new_dataset\new_pipeline\kfold_large_1280_own_lr_0.005\large"
 
-df = pd.DataFrame(columns=['metrics/precision(B)', 'metrics/recall(B)', 'metrics/mAP50(B)', 'metrics/mAP50-95(B)', 'metrics/precision(M)', 'metrics/recall(M)', 'metrics/mAP50(M)', 'metrics/mAP50-95(M)'])
+df = pd.DataFrame(columns=['fold_idx', 'metrics/precision(B)', 'metrics/recall(B)', 'metrics/mAP50(B)', 'metrics/mAP50-95(B)', 'metrics/precision(M)', 'metrics/recall(M)', 'metrics/mAP50(M)', 'metrics/mAP50-95(M)'])
 
 for fold_idx in range(1, 6):
     fold_validation_path = os.path.join(project_path, f"fold_{fold_idx}", "val", "validation_results.json")
     
     with open(fold_validation_path) as file:
         val_dict = json.load(file)
-        
+    
+    val_dict["fold_idx"] = fold_idx
     df.loc[len(df)] = val_dict
         
 df.to_csv(os.path.join(project_path, "fold_results.csv"), index=False)
